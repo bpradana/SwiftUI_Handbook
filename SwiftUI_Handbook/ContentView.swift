fa705ef24457a587186808888f9f1c044622700d
Bintang Pradana Erlangga Putra <bintang.pradana@privyid.tech>
2024-01-09 21:55:29 +0700

    Initial Commit

---
+++ SwiftUI_Handbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Handbook
+//
+//  Created by Bintang Pradana on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Handbook/SwiftUI_HandbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HandbookApp.swift
+//  SwiftUI_Handbook
+//
+//  Created by Bintang Pradana on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HandbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
